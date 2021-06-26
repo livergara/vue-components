149527aa2730985a4ec8e049f484f8eb1c2dbe20
Li Vergara <lizza.novikova@gmail.com>
2021-06-26 20:40:36 +0300

    Создала страничку todo листа

---
+++ src/views/InputTagComponent.vue
@@ -0,0 +1,40 @@
+<template>
+  <div style="padding: 30px">
+    <TagInput v-model="tags" />
+    <br /><br />
+
+    <!--Ucomment to see with features not in blog post -->
+    <!-- <TagInput
+      v-model="tags2"
+      :options="['Hello', 'World']"
+      :allowCustom="false"
+      :showCount="true"
+    /> -->
+
+    <!--Ucomment to see tags synced with component -->
+    <!-- <ul>
+      <li v-for="tag in tags" :key="tag">{{ tag }}</li>
+    </ul> -->
+  </div>
+</template>
+
+<script>
+import TagInput from "../components/InputTag.vue";
+export default {
+  name: "App",
+  components: {
+    TagInput: TagInput,
+  },
+  data() {
+    return {
+      tags: ["Hello", "World"],
+      tags2: ["Hello"],
+    };
+  },
+};
+</script>
+<style>
+* {
+  box-sizing: border-box;
+}
+</style>
---
+++ src/views/TodoListComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <todo-list list-name="Мой список дел" />
+</template>
+
+<script>
+import TodoList from "../ComponentsForToDo/TodoList.vue";
+export default {
+  name: "App",
+  components: {
+    TodoList,
+  },
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
